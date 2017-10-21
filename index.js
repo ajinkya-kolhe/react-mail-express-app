425e3e14fe4485192b1427142d2c4fbd504b5d23
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-10-21 22:28:42 +1100

    Initial passport setup

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStratergy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
+
 const app = express();
 
+passport.use(new GoogleStratergy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(accessToken);
+  console.log(profile);
+}));
+
 //Route Handler
-app.get('/', (request, response) => {
-  response.send({ hi: 'there'});
-});
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+//Redirect URI
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
