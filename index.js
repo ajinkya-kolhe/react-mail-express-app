57ce8ce159a97544f5220b540963a40a23b7de92
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-10-22 00:55:33 +1100

    Connecting mongoose to mongo

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const passportConfig = require('./services/passport');
+const keys = require('./config/keys');
+
+// Add below 2 lines to fix the deprecation errors related to mongoose.
+// Promise and JSON object in connect().
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true,
+});
 
 const app = express();
-
 require('./routes/authRoutes')(app);
 
 const port = process.env.PORT || 3000;
