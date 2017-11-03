52db96a9506308cb6734b9d8702a3729fdf7f76a
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-11-04 04:16:36 +1100

    Stripe checkout component integrated and added some styling to it

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Payments from './Payments';
+
 class Header extends React.Component {
   renderContent() {
     switch (this.props.auth) {
@@ -14,11 +16,12 @@ class Header extends React.Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
   render() {
